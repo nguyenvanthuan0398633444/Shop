ed4dee41bd53695be41631d999cf44d32179e462
@nvThuan <53548696+nguyenvanthuan0398633444@users.noreply.github.com>
2021-04-14 16:50:32 +0700

    Add files via upload

---
+++ comment.js
@@ -0,0 +1,7 @@
+var x = document.getElementsByClassName("item-event");
+
+function ShowContent(){
+    document.getElementById("t").classList.add('show');
+    // document.getElementsById("k").innerHTML = "tung";
+    console.log("thuan ");
+}
